5a060efe4cb9166307cc0c5a63388335b1aa9fd1
Jascuas <javiersanchezdelago@hotmail.com>
2019-10-28 11:42:07 +0100

    Creating store, adding dummy products

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, ownerId, tittle, imageUrl, description, price) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.tittle = tittle;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+}
