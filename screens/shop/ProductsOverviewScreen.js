f616d0c49bd38306e5f96304d06fce07fbc1dff6
Jascuas <javiersanchezdelago@hotmail.com>
2019-10-28 12:43:32 +0100

    Adding navigation

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { StyleSheet, View, Text, FlatList } from "react-native";
+import { useSelector } from "react-redux";
 
 const ProductsOverviewScreen = props => {
-  return <FlatList />;
+  const products = useSelector(state => state.products.availableProducts);
+
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products'
 };
 
 const styles = StyleSheet.create({});
