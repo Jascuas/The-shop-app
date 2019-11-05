9c60e65291df86f9f1eb1bd0a5efeac17e44fad3
Jascuas <javiersanchezdelago@hotmail.com>
2019-11-05 13:32:14 +0100

    Outputing cart items

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,14 +1,18 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { Platform } from "react-native";
+
+
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
 import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
     ProducsOverview: ProductsOverviewScreen,
-    ProductDetail: ProductDetailScreen
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
   },
   {
     defaultNavigationOptions: {
