8262745ed92a7ed6ccbe9050df7bfcb8c7aa25e6
Jascuas <javiersanchezdelago@hotmail.com>
2019-11-06 12:33:20 +0100

    User products screen

---
+++ screens/user/UserProductsScreen.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FlatList, Platform } from "react-native";
+import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import ProductItem from "../../components/shop/ProductItem";
+import HeaderButton from "../../components/UI/HeaderButtom";
+
+const UserProducsScreen = props => {
+  const userProducts = useSelector(state => state.products.userProducts);
+  return (
+    <FlatList
+      data={userProducts}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+UserProducsScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Your Products",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          onPress={() => {
+            navData.navigation.navigate("Cart");
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
+
+export default UserProducsScreen;
